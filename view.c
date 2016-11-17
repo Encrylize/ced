71d1dba092cba27cd0304ae9e972ad563827c60c
Encrylize <encrylize@gmail.com>
2016-11-17 17:20:53 +0100

    Add horizontal scrolling functionality

--- view.c
+++ view.c
@@ -13,6 +13,7 @@ View *view_new(int rows, int cols, int begin_col, int begin_row, char *filename)
         view->buf = buffer_new(filename);
         view->win = newwin(cols, rows, begin_col, begin_row);
         view->top_line = view->buf->root_line;
+        view->top_row = 0;
         view->top_col = 0;
         keypad(view->win, true);
     }
@@ -28,18 +29,26 @@ void view_destroy(View *view) {
 
 bool view_adjust(View *view) {
     size_t cols = getmaxy(view->win) - 1;
+    size_t rows = getmaxx(view->win) - 1;
+    size_t scroll_cols = 0;
+    size_t scroll_rows = 0;
 
     if (view->buf->col > (view->top_col + cols))
-        view_scroll(view, view_get_cursor_col(view) - cols);
+        scroll_cols = view_get_cursor_col(view) - cols;
     else if (view->buf->col < view->top_col)
-        view_scroll(view, view_get_cursor_col(view));
-    else
-        return false;
+        scroll_cols = view_get_cursor_col(view);
 
-    return true;
+    if (view->buf->row > (view->top_row + rows))
+        scroll_rows = view_get_cursor_row(view) - rows;
+    else if (view->buf->row < view->top_row)
+        scroll_rows = view_get_cursor_row(view);
+
+    view_scroll(view, scroll_rows, scroll_cols);
+
+    return scroll_cols != 0 || scroll_rows != 0;
 }
 
-void view_scroll(View *view, int col) {
+void view_scroll(View *view, int row, int col) {
     if (col < 0) {
         /* Move up */
         while (view->top_line->prev != NULL && col < 0) {
@@ -55,6 +64,23 @@ void view_scroll(View *view, int col) {
             col--;
         }
     }
+
+    int new_row = (int) view->top_row + row;
+    if (new_row < 0)
+        view->top_row = 0;
+    else if ((size_t) new_row > view->buf->cur_line->len)
+        view->top_row = view->buf->cur_line->len - 1;
+    else
+        view->top_row = new_row;
+}
+
+void view_redraw_line(View *view, Line *line, int col) {
+    if (line->len > view->top_row)
+        mvwprintw(view->win, col, 0, "%.*s",
+                  getmaxx(view->win), &line->content[view->top_row]);
+    else
+        wmove(view->win, col, 0);
+    wclrtoeol(view->win);
 }
 
 void view_redraw(View *view, Line *line, int col) {
--- view.h
+++ view.h
@@ -7,12 +7,7 @@
 #include "buffer.h"
 
 #define view_get_cursor_col(view) (view->buf->col - view->top_col)
-#define view_redraw_line(view, line, col)                                     \
-    do {                                                                      \
-        mvwprintw(view->win, col, 0, "%s", line->content);                    \
-        wclrtoeol(view->win);                                                 \
-    } while (0)
-
+#define view_get_cursor_row(view) (view->buf->row - view->top_row)
 #define view_redraw_full(view)                                                \
     do {                                                                      \
         view_redraw(view, view->top_line, 0);                                 \
@@ -21,7 +16,8 @@
 
 #define view_redraw_cursor(view)                                              \
     do {                                                                      \
-        wmove(view->win, view_get_cursor_col(view), view->buf->row);          \
+        wmove(view->win,                                                      \
+              view_get_cursor_col(view), view_get_cursor_row(view));          \
         wrefresh(view->win);                                                  \
     } while (0)
 
@@ -30,13 +26,15 @@ typedef struct View {
     WINDOW *win;
     Buffer *buf;
     Line *top_line;
+    size_t top_row;
     size_t top_col;
 } View;
 
 View *view_new(int rows, int cols, int begin_col, int begin_row, char *filename);
 void view_destroy(View *view);
 bool view_adjust(View *view);
-void view_scroll(View *view, int col);
+void view_scroll(View *view, int row, int col);
+void view_redraw_line(View *view, Line *line, int col);
 void view_redraw(View *view, Line *line, int col);
 
 
